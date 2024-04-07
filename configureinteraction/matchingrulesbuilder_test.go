982303fe02078bbd7db77f185412d1f2c7e8d713
Praveen Erode Mohanasundaram <praveen.em@gmail.com>
2024-04-07 23:18:49 +0100

    chore

--- configureinteraction/matchingrulesbuilder_test.go
+++ configureinteraction/matchingrulesbuilder_test.go
@@ -13,9 +13,9 @@ func TestMatchingRulesBuilder(t *testing.T) {
 	t.Parallel() //marks MatchingRulesBuilder as capable of running in parallel with other tests
 
 	type in struct {
-		exampleValue map[string]any
-		matchType map[string]any
-		matchTypeConfig map[string]any
+		exampleValueMap map[string]any
+		matchTypeMap map[string]any
+		matchTypeConfigMap map[string]any
 	}
 
 	type out struct {
@@ -31,58 +31,58 @@ func TestMatchingRulesBuilder(t *testing.T) {
 	}{
 		{
 			name: "RulesType",
-			input: in{exampleValue: map[string]any{"default":"value1"}, matchType: map[string]any{"default": "type"}, matchTypeConfig: map[string]any{} },
+			input: in{exampleValueMap: map[string]any{"default":"value1"}, matchTypeMap: map[string]any{"default": "type"}, matchTypeConfigMap: map[string]any{} },
 			want: out{rules:`[{"type":"type", "values":{"match":"type"}}]` , err: nil},			
 		},
 		{
 			name: "RulesEqualTo",
-			input: in{exampleValue: map[string]any{"default":"value1"}, matchType: map[string]any{"default": "equalTo"}, matchTypeConfig: map[string]any{} },
+			input: in{exampleValueMap: map[string]any{"default":"value1"}, matchTypeMap: map[string]any{"default": "equalTo"}, matchTypeConfigMap: map[string]any{} },
 			want: out{rules:`[{"type":"equalTo", "values":{"match":"equalTo"}}]` , err: nil},			
 		},
 		{
 			name: "RulesDate",
-			input: in{exampleValue: map[string]any{"default":"2023-Jul-08"}, matchType: map[string]any{"default": "date"}, matchTypeConfig: map[string]any{"default": "2006-01-02"} },
+			input: in{exampleValueMap: map[string]any{"default":"2023-Jul-08"}, matchTypeMap: map[string]any{"default": "date"}, matchTypeConfigMap: map[string]any{"default": "2006-01-02"} },
 			want: out{rules:`[{"type":"date","values":{"format":"2006-01-02","match":"date"}}]` , err: nil},			
 		},
 		{
 			name: "RulesDateTime",
-			input: in{exampleValue: map[string]any{"default":"2023-07-21 16:44:32"}, matchType: map[string]any{"default": "datetime"}, matchTypeConfig: map[string]any{"default": "2006-01-02 15:04:05"} },
+			input: in{exampleValueMap: map[string]any{"default":"2023-07-21 16:44:32"}, matchTypeMap: map[string]any{"default": "datetime"}, matchTypeConfigMap: map[string]any{"default": "2006-01-02 15:04:05"} },
 			want: out{rules:`[{"type":"datetime","values":{"format":"2006-01-02 15:04:05","match":"datetime"}}]` , err: nil},			
 		},
 		{
 			name: "RulesTime",
-			input: in{exampleValue: map[string]any{"default":"16:44:32"}, matchType: map[string]any{"default": "time"}, matchTypeConfig: map[string]any{"default": "15:04:05"} },
+			input: in{exampleValueMap: map[string]any{"default":"16:44:32"}, matchTypeMap: map[string]any{"default": "time"}, matchTypeConfigMap: map[string]any{"default": "15:04:05"} },
 			want: out{rules:`[{"type":"time","values":{"format":"15:04:05","match":"time"}}]` , err: nil},			
 		},
 		{
 			name: "RulesRegex",
-			input: in{exampleValue: map[string]any{"default":"v2"}, matchType: map[string]any{"default": "regex"}, matchTypeConfig: map[string]any{"default": "v[a-zA-z0-9]+"} },
+			input: in{exampleValueMap: map[string]any{"default":"v2"}, matchTypeMap: map[string]any{"default": "regex"}, matchTypeConfigMap: map[string]any{"default": "v[a-zA-z0-9]+"} },
 			want: out{rules:`[{"type":"regex","values":{"regex":"v[a-zA-z0-9]+","match":"regex"}}]` , err: nil},			
 		},
 		{
 			name: "RulesBoolean",
-			input: in{exampleValue: map[string]any{"default":true}, matchType: map[string]any{"default": "boolean"}, matchTypeConfig: map[string]any{} },
+			input: in{exampleValueMap: map[string]any{"default":true}, matchTypeMap: map[string]any{"default": "boolean"}, matchTypeConfigMap: map[string]any{} },
 			want: out{rules:`[{"type":"boolean","values":{"match":"boolean"}}]` , err: nil},			
 		},
 		{
 			name: "RulesContentType",
-			input: in{exampleValue: map[string]any{"default":"{}"}, matchType: map[string]any{"default": "contentType"}, matchTypeConfig: map[string]any{"default":"application/avro"} },
+			input: in{exampleValueMap: map[string]any{"default":"{}"}, matchTypeMap: map[string]any{"default": "contentType"}, matchTypeConfigMap: map[string]any{"default":"application/avro"} },
 			want: out{rules:`[{"type":"contentType","values":{"match":"contentType","value":"application/avro"}}]` , err: nil},			
 		},
 		{
 			name: "RulesEachValueType",
-			input: in{exampleValue: map[string]any{"eachValue":100}, matchType: map[string]any{"eachValue": "type"}, matchTypeConfig: map[string]any{} },
+			input: in{exampleValueMap: map[string]any{"eachValue":100}, matchTypeMap: map[string]any{"eachValue": "type"}, matchTypeConfigMap: map[string]any{} },
 			want: out{rules:`[{"type":"eachValue","values":{"rules":[{"match":"type"}],"value":100}}]` , err: nil},			
 		},
 		//TODO: Needs rethink on want for RulesEachValueReference
 		{
 			name: "RulesEachValueReference",
-			input: in{exampleValue: map[string]any{"reference": true, "eachValue":"subProducts"}, matchType: map[string]any{"reference": "values", "eachValue":"$"}, matchTypeConfig: map[string]any{} },
+			input: in{exampleValueMap: map[string]any{"reference": true, "eachValue":"subProducts"}, matchTypeMap: map[string]any{"reference": "values", "eachValue":"$"}, matchTypeConfigMap: map[string]any{} },
 			want: out{rules:`[{"type":"values","values":{"match":"values"}}]` , err: nil},			
 		},
 		{
 			name: "RulesEachKeyEachValue",
-			input: in{exampleValue: map[string]any{"eachKey":"key1", "eachValue":"value1"}, matchType: map[string]any{"eachKey":"type", "eachValue":"notEmpty"}, matchTypeConfig: map[string]any{} },
+			input: in{exampleValueMap: map[string]any{"eachKey":"key1", "eachValue":"value1"}, matchTypeMap: map[string]any{"eachKey":"type", "eachValue":"notEmpty"}, matchTypeConfigMap: map[string]any{} },
 			want: out{rules:`[{"type":"eachKey","values":{"rules":[{"match":"type"}],"value":"key1"}},{"type":"eachValue","values":{"rules":[{"match":"notEmpty"}],"value":"value1"}}]` , err: nil},			
 		},
 	}
@@ -96,7 +96,7 @@ func TestMatchingRulesBuilder(t *testing.T) {
 			got := out{}
 
 			//Act
-			rules, err := buildMatchingRules(test.input.matchType, test.input.matchTypeConfig, test.input.exampleValue)			 
+			rules, err := buildMatchingRules(test.input.matchTypeMap, test.input.matchTypeConfigMap, test.input.exampleValueMap)			 
 			got.err = err
 
 			bytes, err := json.Marshal(rules.GetRule())			
@@ -111,7 +111,7 @@ func TestMatchingRulesBuilder(t *testing.T) {
 
 			//Assert
 			require.ErrorIs(t, got.err, test.want.err)				
-			assert.JSONEq(t, test.want.rules , got.rules)
+			assert.JSONEq(t, test.want.rules , got.rules) //sometimes fail, same problem as https://github.com/stretchr/testify/issues/1025. needs fixing.
 		})
 	}
 	
